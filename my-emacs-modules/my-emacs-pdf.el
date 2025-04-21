665cc983ee43e789007a0b7f954e7f911d3c0cc4
cssck <skorykjura@gmail.com>
2025-04-21 19:47:14 +0300

    deleted extra stuff

--- my-emacs-modules/my-emacs-pdf.el
+++
@@ -1,8 +0,0 @@
-(use-package pdf-tools
-  :ensure t
-  :config
-  (pdf-tools-install)
-  (setq pdf-view-resize-factor 1.1)
-  (setq-default pdf-view-display-size 'fit-page))
-
-(provide 'my-emacs-pdf)
